5ff1ac59a020512aed098ae0347762375edaafc2
subin <github@subin.kr>
2024-06-18 09:32:06 +0900

    FIX: Typing error

--- dags/dags_bash_trigger_rule.py
+++ dags/dags_bash_trigger_rule.py
@@ -13,22 +13,25 @@ with DAG(
         task_id = 't1',
         bash_command='exit 0',
         dag=dag
-    ),
+    )
+
     t2 = BashOperator(
         task_id = 't2',
         bash_command='exit 1',
         dag=dag
-    ),
+    )
+
     t3 = BashOperator(
         task_id = 't3',
         bash_command='exit 1',
         dag=dag
-    ),
+    )
+
     t4 = BashOperator(
         task_id = 't4',
         bash_command='echo "last task"',
         trigger_rule='one_success',
         dag=dag
-    ),
+    )
     
     [t1, t2, t3] >> t4
